232d81a0afdf31928f0d459d22e2ff83ff49ca38
igrgin <ivo.grgin@outlook.com>
2025-07-31 01:30:28 +0200

    Extract URL Path

--- app/main.go
+++ app/main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 var _ = net.Listen
 var _ = os.Exit
+
+const CRLF = "\r\n"
 
 func main() {
 
@@ -24,5 +27,27 @@ func main() {
 	}
 
 	fmt.Println("Connection accepted from", conn.RemoteAddr())
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	buffer := make([]byte, 1024)
+	_, err = conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+	}
+	fmt.Println("Received data:", string(buffer))
+
+	req := string(buffer)
+	lines := strings.Split(req, CRLF)
+	path := strings.Split(lines[0], " ")[1]
+	fmt.Println(path)
+	var res string
+	if path == "/" {
+		res = "HTTP/1.1 200 OK\r\n\r\n"
+	} else {
+		res = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+	fmt.Println(res)
+	conn.Write([]byte(res))
+
+	conn.Close()
+	l.Close()
+
 }
