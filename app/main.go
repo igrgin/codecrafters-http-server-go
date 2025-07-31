888c4c60b1e71ca5ce390233258b15cba8c843ce
igrgin <ivo.grgin@outlook.com>
2025-07-31 02:11:42 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,53 +1,152 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
-var _ = net.Listen
-var _ = os.Exit
-
-const CRLF = "\r\n"
+// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
+var (
+	_ = net.Listen
+	_ = os.Exit
+)
 
 func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
 
+	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	fmt.Println("Connection accepted from", conn.RemoteAddr())
-	buffer := make([]byte, 1024)
-	_, err = conn.Read(buffer)
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handle(conn)
+	}
+}
+
+type Request struct {
+	Method   string
+	Path     string
+	Protocol string
+	Headers  map[string]string
+	Body     string
+}
+
+func handle(connection net.Conn) net.Conn {
+	reader := bufio.NewReader(connection)
+
+	response, _ := reader.ReadString('\n')
+
+	requestParts := strings.Split(response, " ")
+
+	request := Request{
+		Method:   requestParts[0],
+		Path:     requestParts[1],
+		Protocol: strings.TrimSuffix(requestParts[2], "\r\n"),
+		Headers:  make(map[string]string),
+		Body:     "",
+	}
+
+	for {
+		header, _ := reader.ReadString('\n')
+		if header == "\r\n" {
+			break
+		}
+		headerParts := strings.Split(header, ":")
+		request.Headers[headerParts[0]] = strings.TrimSpace(strings.Join(headerParts[1:], ""))
+	}
+	contentSize, err := strconv.Atoi(request.Headers["Content-Length"])
+	fmt.Println(err)
+	if err == nil {
+		content := make([]byte, contentSize)
+		size, err := io.ReadFull(reader, content)
+		if size != contentSize {
+			log.Fatal("dead")
+		}
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			request.Body = string(content)
+		}
+	}
+	s, err := json.Marshal(request)
 	if err != nil {
-		fmt.Println("Error reading from connection: ", err.Error())
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	fmt.Println("Received data:", string(buffer))
+	fmt.Println(string(s))
+	connection = handleRequest(request, connection)
 
-	req := string(buffer)
-	lines := strings.Split(req, CRLF)
-	path := strings.Split(lines[0], " ")[1]
-	fmt.Println(path)
-	var res string
-	if path == "/" {
-		res = "HTTP/1.1 200 OK\r\n\r\n"
-	} else {
-		res = "HTTP/1.1 404 Not Found\r\n\r\n"
+	return connection
+}
+
+func handleRequest(request Request, connection net.Conn) net.Conn {
+	defer connection.Close()
+
+	switch {
+	case request.Method == "GET":
+		{
+			handleGet(request, connection)
+		}
+	default:
+		{
+			errors.New("Unhandled Method")
+		}
 	}
-	fmt.Println(res)
-	conn.Write([]byte(res))
+	return connection
+}
 
-	conn.Close()
-	l.Close()
+func handleGet(request Request, connection net.Conn) net.Conn {
+	switch {
+	case request.Path == "/":
+		{
+			connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		}
+	case strings.HasPrefix(request.Path, "/echo/"):
+		{
+			var sb strings.Builder
+			response, _ := strings.CutPrefix(request.Path, "/echo/")
+			sb.WriteString(response)
+			strResponse := fmt.Sprintf("HTTP/1.1 200 OK\r\n"+
+				"Content-Type: text/plain\r\n"+
+				"Content-Length: %d\r\n"+
+				"Connection: close\r\n\r\n"+
+				"%s",
+				len(sb.String()),
+				sb.String())
+			fmt.Println(sb.String())
+			fmt.Printf("Response body: [% x]\n", []byte(sb.String()))
+			connection.Write([]byte(strResponse))
+		}
+	default:
+		{
+			connection.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		}
 
+	}
+
+	return connection
 }
